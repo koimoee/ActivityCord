2bd23451f59ee8800335c59557688e288ae69ea3
mikaaamoe <mikaaa.moe>
2023-09-19 10:42:20 +0700

    fix(playing.js): fix formatting and syntax errors in playing.js file feat(playing.js): set activity for client user to "Playing" a game with a placeholder name
    Signed-off-by: mikaaamoe <mikaaa.moe>

--- statuses/playing.js
+++ statuses/playing.js
@@ -1,12 +1,17 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const chalk = require("chalk");
-const TOKEN = (process.env.TOKEN);
-const { Client } = require('discord.js-selfbot-v11')
+const TOKEN = process.env.TOKEN;
+const { Client } = require("discord.js-selfbot-v11");
 const client = new Client();
 
-client.on('ready', () => { 
-  client.user.setActivity("Here Name Of The Games", { type: "PLAYING"})
-   console.log(("[" + chalk.yellowBright.bold("PLAYING") + "]") + ` Successfully logged in as ${client.user.username} (${client.user.id})!`);
-})
+client.on("ready", () => {
+  client.user.setActivity("Here Name Of The Games", { type: "PLAYING" });
+  console.log(
+    "[" +
+      chalk.yellowBright.bold("PLAYING") +
+      "]" +
+      ` Successfully logged in as ${client.user.username} (${client.user.id})!`
+  );
+});
 
 client.login(TOKEN);
