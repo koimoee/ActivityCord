969f5c8cd76cca47c219da4ba90a72964a232709
mikaaamoe <mikaaa.moe>
2023-09-19 10:39:10 +0700

    chore(index.js): format code for better readability feat(index.js): add line breaks and indentation for console log statements refactor(index.js): change prompt sync import statement to multi-line format feat(index.js): add line breaks and indentation for server route feat(index.js): add line breaks and indentation for playing status import statement feat(index.js): add line breaks and indentation for listening status import statement feat(index.js): add line breaks and indentation for streaming status import statement feat(index.js): add line breaks and indentation for invalid option console log statement
    Signed-off-by: mikaaamoe <mikaaa.moe>

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const express = require("express");
 const chalk = require("chalk");
 const server = express();
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-console.log(chalk.cyanBright.bold("ActivityCord") + " | " + chalk.greenBright.bold("RinForeverz - Rin4Ever.xyz"));
+console.log(
+  chalk.cyanBright.bold("ActivityCord") +
+    " | " +
+    chalk.greenBright.bold("RinForeverz - Rin4Ever.xyz")
+);
 
 server.all("/", (req, res) => {
-  res.send('<meta http-equiv="refresh" content="0; URL=https://rin4ever.xyz"/>')
-})
+  res.send(
+    '<meta http-equiv="refresh" content="0; URL=https://rin4ever.xyz"/>'
+  );
+});
 server.listen(3000);
-console.log(("\n[" + chalk.green.bold("+") + "]") + " The webserver is ready!.");
+console.log("\n[" + chalk.green.bold("+") + "]" + " The webserver is ready!.");
 
-console.log("\n[" + chalk.yellow.bold("!") + "] Which presence would you like to start?\n\n[1] Playing Status \n[2] Listening Status\n[3] Streaming Status\n\n");
+console.log(
+  "\n[" +
+    chalk.yellow.bold("!") +
+    "] Which presence would you like to start?\n\n[1] Playing Status \n[2] Listening Status\n[3] Streaming Status\n\n"
+);
 
 status = prompt("> ");
 
 if (status == 1) {
   console.clear();
-  import('./statuses/playing.js');
+  import("./statuses/playing.js");
 } else if (status == 2) {
   console.clear();
-  import('./statuses/listening.js');
+  import("./statuses/listening.js");
 } else if (status == 3) {
   console.clear();
-  import('./statuses/streaming.js');
+  import("./statuses/streaming.js");
 } else {
-  console.log("[" + chalk.red.bold("-") + "] Invalid option, Select Options First!");
+  console.log(
+    "[" + chalk.red.bold("-") + "] Invalid option, Select Options First!"
+  );
   process.exit();
 }
